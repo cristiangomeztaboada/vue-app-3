ba536dcb2b81938b18d7e9a20adde2f26cf7c8e7
Cristian Gomez Taboada <cristian.gomez.taboada@hotmail.com>
2022-05-24 00:41:40 -0500

    CDP

--- src/views/certificadoPresupuesto/CertificadoPresupuestoFormulario.vue
+++ src/views/certificadoPresupuesto/CertificadoPresupuestoFormulario.vue
@@ -42,6 +42,7 @@
                 v-model="fecha"
                 type="date"
                 id="fecha"
+                readonly
               />
             </div>
             <div class="col-sm-3 col-md-3 col-lg-3 col-xl-3">
